bf91ddfba16bb077a3a03cac1ca5b559f870a520
3lfive <denystry@gmail.com>
2021-12-10 15:59:16 +0200

    some styling

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './card.style.css'
 export const Card = props => {
   return (
-    <div>
+    <div className='card-container'>
     <h1> {props.monsters.name} < /h1>
     </div>
   )
